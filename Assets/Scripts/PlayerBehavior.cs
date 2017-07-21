cc2161821ebc691150f40fb16a34e2d81a5c4e1a
Theo Lipeles <Theo@Lipeles.com>
2017-07-21 11:55:47 -0400

    changes the sizes of player and bullets

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -14,20 +14,20 @@ public class PlayerBehavior : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		playerVelocity = 3;
-		bulletVelocity = 6;
+		playerVelocity = 0.1f;
+		bulletVelocity = 0.3f;
 		cam = Camera.main;
 	}
 
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.LeftShift)) {playerVelocity = 1;}
-		float yPos = gameObject.transform.position.y + (Input.GetAxis ("Vertical")) * Time.deltaTime * playerVelocity;
-		float xPos = gameObject.transform.position.x + (Input.GetAxis ("Horizontal")) * Time.deltaTime * playerVelocity;
+		if (Input.GetKey(KeyCode.LeftShift)) {playerVelocity = 0.05f;}
+		float yPos = gameObject.transform.position.y + (Input.GetAxis ("Vertical")) * playerVelocity;
+		float xPos = gameObject.transform.position.x + (Input.GetAxis ("Horizontal")) * playerVelocity;
 		playerPos = new Vector2 (Mathf.Clamp (xPos, -8, 8), Mathf.Clamp (yPos, -4, 4));
 		gameObject.transform.position = playerPos;
-		playerVelocity = 3;
+		playerVelocity = 0.1f;
 
 		if (Input.GetButton("Fire1")) {
 			float camDis = cam.transform.position.y - playerPos.y;
@@ -38,7 +38,7 @@ public class PlayerBehavior : MonoBehaviour {
 		for (int i = 0; i < Bullets.Count; i++) {
 			GameObject moveBullet = Bullets[i];
 			if (moveBullet != null) {
-				moveBullet.transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * bulletVelocity);
+				moveBullet.transform.Translate(new Vector3(0, 1, 0) * bulletVelocity);
 				Vector2 bulletScreenPos = cam.WorldToScreenPoint(moveBullet.transform.position);
 				if (bulletScreenPos.x > Screen.width || bulletScreenPos.y > Screen.height || bulletScreenPos.y < 0 || bulletScreenPos.x < 0) {
 					DestroyObject(moveBullet);
