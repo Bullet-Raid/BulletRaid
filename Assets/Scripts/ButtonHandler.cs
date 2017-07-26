13985ed35e862898f7a0033b4ada3f99cb09fd41
Theo Lipeles <Theo@Lipeles.com>
2017-07-26 10:45:47 -0400

    Adds basic main menu

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour {
+
+	public void NewGameBtn() {
+		SceneManager.LoadScene("Game");
+	}
+	public void QuitGameBtn() {
+		Application.Quit();
+	}
+}
