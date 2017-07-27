848eb2151d2cf573317b450ed4777224b3dc49cb
Theo Lipeles <Theo@Lipeles.com>
2017-07-27 19:29:19 -0400

    Creates ship class

---
+++ Assets/Scripts/Game Mechanics/Ship.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+	private float Speed;
+	private List<Burst> Bursts;
+	private int CurrentBurstIndex;
+	private int cooldown = 1;
+
+	public Vector3 Position;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	Vector3 Move(Vector3 movementVector) {
+
+	}
+
+	void FireBurst(Vector3 direction, GameObject bulletPrefab) {
+
+	}
+}
