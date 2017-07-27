d59bc6aeeaecef59d4272fdb9220bf604888e25f
Theo Lipeles <Theo@Lipeles.com>
2017-07-27 19:04:54 -0400

    Removes unnecessary namespace "BurstFire"

--- Assets/Scripts/Game Mechanics/Burst.cs
+++ Assets/Scripts/Game Mechanics/Burst.cs
@@ -1,41 +1,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace BurstFire {
+public class Burst {
+	public List<float> shots;
+	public float spread;
+	public int cooldown;
 
-	public class Burst {
-		public List<float> shots;
-		public float spread;
-		public int cooldown;
+	// Constructor
+	public Burst(int newSize = 1, float newSpread = 45f) {
+		shots = MakeSpread(newSize, newSpread);
+		cooldown = newSize * 5;
+	}
 
-		// Constructor
-		public Burst(int newSize = 1, float newSpread = 45f) {
-			shots = MakeSpread(newSize, newSpread);
-			cooldown = newSize * 5;
+	public void ChangeAngle(int index, float newAngle) {
+		if (Mathf.Abs(newAngle) > (spread / 2)) {
+			throw new System.ArgumentException("Angle exceeds maximum spread");
 		}
+		shots[index] = newAngle;
+	}
 
-		public void ChangeAngle(int index, float newAngle) {
-			if (Mathf.Abs(newAngle) > (spread / 2)) {
-				throw new System.ArgumentException("Angle exceeds maximum spread");
-			}
-			shots[index] = newAngle;
-		}
-
-		// Helper functions
-		private List<float> MakeSpread(int newSize, float newSpread) {
-			List<float> newShots = new List<float>(newSize);
-			if (newSize == 1) {
-				newShots.Add(0);
-				return newShots;
-			}
-			float midway = newSpread / 2;
-			float spacing = newSpread / (newSize - 1);
-			float currSpread = newSpread;
-			for (int i = 0; i < newSize; i++) {
-				newShots.Add(currSpread - (i * spacing) - midway);
-			}
+	// Helper functions
+	private List<float> MakeSpread(int newSize, float newSpread) {
+		List<float> newShots = new List<float>(newSize);
+		if (newSize == 1) {
+			newShots.Add(0);
 			return newShots;
 		}
-
+		float midway = newSpread / 2;
+		float spacing = newSpread / (newSize - 1);
+		float currSpread = newSpread;
+		for (int i = 0; i < newSize; i++) {
+			newShots.Add(currSpread - (i * spacing) - midway);
+		}
+		return newShots;
 	}
+
 }
--- Assets/Scripts/Game Mechanics/PlayerBehavior.cs
+++ Assets/Scripts/Game Mechanics/PlayerBehavior.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using BurstFire;
 
 public class PlayerBehavior : MonoBehaviour {
 
