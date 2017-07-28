74d35ff5f40f48d4bf6040919ca2915ce95f7980
Theo Lipeles <Theo@Lipeles.com>
2017-07-28 17:28:09 -0400

    fixes Exceeds and MoveClamped methods

--- Assets/Scripts/Game Mechanics/Boundary.cs
+++ Assets/Scripts/Game Mechanics/Boundary.cs
@@ -13,14 +13,17 @@ public class Boundary : MonoBehaviour {
 	}
 
 	public Vector3 MoveClamped(Vector3 start, Vector3 movement) {
-		if (!Exceeds(start + movement)) {return start + movement;}
+		return new Vector3(
+			Mathf.Clamp(start.x + movement.x, LowerLeft.x, UpperRight.x),
+			Mathf.Clamp(start.y + movement.y, LowerLeft.y, UpperRight.y),
+			0);
 	}
 
 	public bool Exceeds(Vector3 v) {
 		return (
-			v.x < UpperRight.x &&
-			v.y < UpperRight.y &&
-			v.x > LowerLeft.x &&
+			v.x < UpperRight.x ||
+			v.y < UpperRight.y ||
+			v.x > LowerLeft.x ||
 			v.x > LowerLeft.y
 		);
 	}
