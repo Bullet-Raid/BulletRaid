2949ff6cd4b0d58c29d211e40572e0736432b221
Theo Lipeles <Theo@Lipeles.com>
2017-07-28 12:42:22 -0400

    Adds Boundary class

---
+++ Assets/Scripts/Game Mechanics/Boundary.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour {
+
+	private Vector3 LowerLeft;
+	private Vector3 UpperRight;
+
+	public Boundary(Vector3 lowerLeft, Vector3 upperRight) {
+		LowerLeft = lowerLeft;
+		UpperRight = upperRight;
+	}
+
+	public Vector3 MovementIntersection(Vector3 start, Vector3 movement) {
+
+	}
+
+	public bool Exceeds(Vector3 v) {
+		return (
+			v.x < UpperRight.x &&
+			v.y < UpperRight.y &&
+			v.x > LowerLeft.x &&
+			v.x > LowerLeft.y
+		);
+	}
+}
