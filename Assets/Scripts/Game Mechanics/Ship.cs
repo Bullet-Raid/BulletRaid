55bd902d575a44643a4efb5e48d7e47fb994ad2f
Gabriel <gabriel.wrowe@gmail.com>
2017-08-08 16:07:16 -0400

    creates: fireburst method, extends MonoBehaviour

--- Assets/Scripts/Game Mechanics/Ship.cs
+++ Assets/Scripts/Game Mechanics/Ship.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ship {
+public class Ship : MonoBehaviour {
 
 	private List<Burst> Bursts;
 	private int CurrentBurstIndex;
@@ -44,6 +44,27 @@ public class Ship {
 	}
 
 	public void FireBurst(Vector3 direction, GameObject bulletPrefab) {
+		Vector3 quaternionDefaultVector = new Vector3(0,1,0);
+		Vector3 translatedPosition = direction - Position;
+		Vector3 zAxis = new Vector3(0,0,1);
 
+		float middleRay = Vector3.SignedAngle(
+			quaternionDefaultVector,
+			translatedPosition,
+			zAxis
+		);
+
+		float bulletRotation;
+		Quaternion bulletDirection;
+		Vector3 radiusAddition;
+		Burst currentBurst = Bursts[CurrentBurstIndex];
+		for(int i = 0; i < currentBurst.shots.Count; i++) {
+			bulletRotation = middleRay + currentBurst.shots[i];
+			bulletDirection = Quaternion.Euler(0, 0, bulletRotation);
+			Instantiate(bulletPrefab, Position, bulletDirection);
+		}
 	}
 }
+
+
+// radiusAddition = direction * (new Vector3(0, 0.1f, 0));
