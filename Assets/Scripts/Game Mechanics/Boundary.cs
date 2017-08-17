4ba33c485394ef325700e0197c9b686dfee67d14
Theo Lipeles <Theo@Lipeles.com>
2017-08-17 18:00:10 -0400

    formats files via autoformat

--- Assets/Scripts/Game Mechanics/Boundary.cs
+++ Assets/Scripts/Game Mechanics/Boundary.cs
@@ -2,25 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boundary {
+public class Boundary
+{
 
 	private Vector3 LowerLeft;
 	private Vector3 UpperRight;
 
-	public Boundary(Vector3 lowerLeft, Vector3 upperRight, float buffer = 0) {
+	public Boundary(Vector3 lowerLeft, Vector3 upperRight, float buffer = 0)
+	{
 		Vector3 bufferVector = new Vector3(buffer, buffer, 0);
 		LowerLeft = lowerLeft + bufferVector;
 		UpperRight = upperRight - bufferVector;
 	}
 
-	public Vector3 MoveClamped(Vector3 start, Vector3 movement) {
+	public Vector3 MoveClamped(Vector3 start, Vector3 movement)
+	{
 		return new Vector3(
 			Mathf.Clamp(start.x + movement.x, LowerLeft.x, UpperRight.x),
 			Mathf.Clamp(start.y + movement.y, LowerLeft.y, UpperRight.y),
 			0);
 	}
 
-	public bool Exceeds(Vector3 v) {
+	public bool Exceeds(Vector3 v)
+	{
 		return (
 			v.x > UpperRight.x ||
 			v.y > UpperRight.y ||
@@ -29,7 +33,8 @@ public class Boundary {
 		);
 	}
 
-	public Vector3 RandomLocation() {
+	public Vector3 RandomLocation()
+	{
 		return new Vector3(
 			Random.Range(LowerLeft.x, UpperRight.x),
 			Random.Range(LowerLeft.y, UpperRight.y));
--- Assets/Scripts/Game Mechanics/Burst.cs
+++ Assets/Scripts/Game Mechanics/Burst.cs
@@ -1,35 +1,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Burst {
+public class Burst
+{
 	public List<float> shots;
 	private float spread;
 	public int cooldown;
 
 	// Constructor
-	public Burst(int newSize = 1, float newSpread = 45f) {
+	public Burst(int newSize = 1, float newSpread = 45f)
+	{
 		shots = MakeSpread(newSize, newSpread);
 		cooldown = newSize * 5;
 	}
 
-	public void ChangeAngle(int index, float newAngle) {
-		if (Mathf.Abs(newAngle) > (spread / 2)) {
+	public void ChangeAngle(int index, float newAngle)
+	{
+		if (Mathf.Abs(newAngle) > (spread / 2))
+		{
 			throw new System.ArgumentException("Angle exceeds maximum spread");
 		}
 		shots[index] = newAngle;
 	}
 
 	// Helper functions
-	private List<float> MakeSpread(int newSize, float newSpread) {
+	private List<float> MakeSpread(int newSize, float newSpread)
+	{
 		List<float> newShots = new List<float>(newSize);
-		if (newSize == 1) {
+		if (newSize == 1)
+		{
 			newShots.Add(0);
 			return newShots;
 		}
 		float midway = newSpread / 2;
 		float spacing = newSpread / (newSize - 1);
 		float currSpread = newSpread;
-		for (int i = 0; i < newSize; i++) {
+		for (int i = 0; i < newSize; i++)
+		{
 			newShots.Add(currSpread - (i * spacing) - midway);
 		}
 		return newShots;
