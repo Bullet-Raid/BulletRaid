0072d313fcabe9751d4905dd7276fdacca01cecc
BuildTools <justinschwaitzberg@gmail.com>
2017-08-21 14:49:33 -0400

    Created PatternManager.cs

---
+++ Assets/Scripts/Game Mechanics/PatternManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternManager : MonoBehaviour {
+
+	GameObject enemyPrefab;
+
+	public PatternManager(GameObject enemyPrefabConstructor){
+		enemyPrefab = enemyPrefabConstructor;
+	}
+
+
+	void Start () {
+
+
+	}
+
+
+	void CreateEnemyPattern(int x, int y, int count, float variability){
+		for(int i = 0; i < count; i++){
+			
+		}
+
+
+
+	}
+	
+}
