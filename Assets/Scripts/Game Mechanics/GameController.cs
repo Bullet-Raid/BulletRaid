a8cd093739777d9e5c30554c099b9c0ab4f2744b
BuildTools <justinschwaitzberg@gmail.com>
2017-08-21 14:52:20 -0400

    Fixed GameController file in branch

---
+++ Assets/Scripts/Game Mechanics/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+	public GameObject enemyPrefab;
+	public GameObject playerPrefab;
+
+	private int spawnTimer;
+
+	// Use this for initialization
+	void Start()
+	{
+		spawnTimer = 0;
+		Instantiate(playerPrefab);
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (spawnTimer == 100)
+		{
+			Debug.Log("Instantiated New Enemy");
+			spawnTimer = 0;
+			Instantiate(enemyPrefab);
+		}
+		else
+		{
+
+			//Debug.Log("Spawn Timer: " + spawnTimer);
+			spawnTimer++;
+		}
+
+	}
+}
